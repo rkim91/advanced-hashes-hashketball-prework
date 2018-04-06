fd5611e56167b4d8625516c7a1d376c0fa25c73c
Richard Kim <richeekim@gmail.com>
2018-04-06 01:40:10 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -152,7 +152,7 @@ def player_numbers(teamname)
   arr = []
   game_hash.each do |location, team_data|
     if game_hash[location][:team_name].include?(teamname)
-      arr << game_hash[location][:team_name][:player][:number].to_i
+      arr << game_hash[location][:team_name][:player][:number]
     end
   end
   return arr
